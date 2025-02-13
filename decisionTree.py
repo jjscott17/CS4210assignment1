8980366abcc0a63bae56925190d1561bf1a6d59c
Joseph Scott <165531069+jjscott17@users.noreply.github.com>
2025-02-12 21:19:36 -0800

    Improved readability

--- decisionTree.py
+++ decisionTree.py
@@ -27,29 +27,32 @@ with open('contact_lens.csv', 'r') as csvfile:
 #--> add your Python code here
 # X =
 
-# use dictionaries to transform the features
+# use dictionaries to transform the features into numerical values
 age_map = {'Young': 1, 'Prepresbyopic': 2, 'Presbyopic': 3}
 spectacle_map = {'Myope': 1, 'Hypermetrope': 2}
 astigmatism_map = {'Yes': 1, 'No': 2}
 tear_map = {'Normal': 1, 'Reduced': 2}
-class_map = {'Yes': 1, 'No': 2}
 
-# row[0]
+# append values associated with each feature to X
+# append values associated with class labels to Y
 for row in db:
     X.append([age_map[row[0]], spectacle_map[row[1]], astigmatism_map[row[2]], tear_map[row[3]]])
-    Y.append(class_map[row[4]])
 
+
+# print X and Y for verification
 for i in X:
     print(i)
-print(Y)
-
-
-
-# if read from file is young or soemthing, append 1,, etc
 
 #transform the original categorical training classes into numbers and add to the vector Y. For instance Yes = 1, No = 2
 #--> addd your Python code here
 # Y =
+
+# use a dictionary to transform class label to numerical values
+class_map = {'Yes': 1, 'No': 2}
+
+for row in db:
+    Y.append(class_map[row[4]])
+print(Y)
 
 #fitting the decision tree to the data
 clf = tree.DecisionTreeClassifier(criterion = 'entropy')
